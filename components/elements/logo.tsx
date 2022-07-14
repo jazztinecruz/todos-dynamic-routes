de217839d7c6c6d6ed7c72ec265828246ee81f64
jazztinecruz <jzztn.crz@gmail.com>
2022-07-14 15:02:16 +0800

    Change logo and linked to home page

--- components/elements/logo.tsx
+++ components/elements/logo.tsx
@@ -1,6 +1,12 @@
+import Link from 'next/link'
+
 const Logo = () => {
   return (
-    <h1 className='font-black text-xl tracking-wide'>Time<span className='text-blue-700'>Table</span></h1>
+    <Link href={'/'}>
+      <h1 className="font-black text-xl tracking-wide cursor-pointer">
+        Todo<span className="text-blue-700">List</span>
+      </h1>
+    </Link>
   )
 }
 
