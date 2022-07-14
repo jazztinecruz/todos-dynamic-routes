530b8d1743b54216400d6a2b0f2fef108247f064
jazztinecruz <jzztn.crz@gmail.com>
2022-07-14 15:02:46 +0800

    Add todo ui line by color

--- components/section/todo/index.tsx
+++ components/section/todo/index.tsx
@@ -17,17 +17,21 @@ const Todo = ({ todo }: Props) => {
   }, [])
 
   return (
-    <div
-      className="grid grid-cols-[1fr,auto] items-center border-[1px] border-gray-200 rounded-md py-3 px-6 hover:bg-slate-100 cursor-pointer"
-    >
-      <p className="text-md fotn-medium tracking-wide">{todo.title}</p>
-      <span
-        className={`${
-          completeness === 'Completed' ? 'text-green-700' : 'text-red-700'
-        } font-medium text-sm`}
-      >
-        {completeness}
-      </span>
+    <div className='grid grid-cols-[auto,1fr] border-[1px] border-gray-200 rounded-md  hover:bg-slate-100 cursor-pointer'>
+      {/* line */}
+      <div className={`${completeness === 'Completed' ? "bg-green-700" : "bg-red-700"} w-1 h-full rounded-md`}></div>
+
+      {/* todo details */}
+      <div className="grid grid-cols-[1fr,auto] items-center py-3 px-6">
+        <p className="text-md fotn-medium tracking-wide">{todo.title}</p>
+        <span
+          className={`${
+            completeness === 'Completed' ? 'text-green-700' : 'text-red-700'
+          } font-medium text-sm`}
+        >
+          {completeness}
+        </span>
+      </div>
     </div>
   )
 }
